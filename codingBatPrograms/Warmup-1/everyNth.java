9e5beb518aee7e48cc13d603d430616ac09e41b9
Daniel Wright <daniel.wright.314@gmail.com>
2019-04-09 14:19:51 -0700

    fixed typo

--- codingBatPrograms/Warmup-1/everyNth.java
+++ codingBatPrograms/Warmup-1/everyNth.java
@@ -1,6 +1,6 @@
 /*
 * Given a non-empty string and an int N, return the string made starting with
-8 char 0, and then every Nth char of the string. So if N is 3, use char 0, 3, 6,
+* char 0, and then every Nth char of the string. So if N is 3, use char 0, 3, 6,
 *  ... and so on. N is 1 or more.
 *
 * everyNth("Miracle", 2) → "Mrce"
